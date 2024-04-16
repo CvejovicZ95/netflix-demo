3dac42aa565d8f0ea4ecc81170e684354abd0f5a
Zoran Cvejovic <cvejovicz@gmail.com>
2024-04-16 23:05:35 +0200

    no need for type module in package.json

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,19 +3,35 @@ module.exports = {
     browser: true,
     node: true,
     es6: true,
-    jest: true
+    jest: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    ecmaFeatures: { jsx: true },
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        // @ts-ignore
+        paths: [__dirname],
+      },
+    },
+  },
   rules: {
     "prettier/prettier": [
       "error",
       {
-        endOfLine: "auto"
-      }
-    ]
-  }
-}
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
